c0e787b11dbabc29a5a681a95b6063c4536be27e
jack middleton hello@jackmiddleton.co <hello@jackmiddleton.co>
2021-04-22 20:36:37 +0100

    done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,47 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:show,:edit,:update,:destroy]
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    #create a movie with the data of the form
+    # save the form
+    @task.save
+    # redirect back to index page
+    redirect_to tasks_path
+  end
+
+  def index #all tasks
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to @task
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #CRUD
+  #verb 'path' to: 'controller#action'
+
+  #CREATE
+  get '/tasks/new', to: 'tasks#new'
+
+  #READ all
+  get '/tasks', to: 'tasks#index'
+
+  #READ one
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  #UPDATE
+  get '/tasks/:id/edit', to: 'tasks#edit'
+  patch '/tasks/:id', to: 'tasks#update'
+
+  #DELETE
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
